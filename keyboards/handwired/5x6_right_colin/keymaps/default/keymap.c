a4e218cb66d243a643145c78ac19db089835a8fc
cchosch <colinhoscheit@gmail.com>
2021-12-24 18:04:15 -0500

    left keyboard added functionality

--- keyboards/handwired/5x6_right_colin/keymaps/default/keymap.c
+++ keyboards/handwired/5x6_right_colin/keymaps/default/keymap.c
@@ -28,35 +28,35 @@ enum layer_names {
 
 const uint16_t PROGMEM keymaps[3][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
-    [_BASE] = COLIN_RIGHT_DACTYL_LAYOUT( 
-        KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC, 
-        KC_Y, KC_U, KC_I, KC_O, KC_P, KC_QUOT, 
-        KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_ENT, 
-        KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_NO, 
+    [_BASE] = COLIN_RIGHT_DACTYL_LAYOUT(
+        KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC,
+        KC_Y, KC_U, KC_I, KC_O, KC_P, KC_QUOT,
+        KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_ENT,
+        KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_BSLS,
                   KC_PMNS, KC_PPLS,
-          KC_SPC, MO(_FN), 
-    KC_NO, KC_NO, 
-    KC_NO, KC_NO 
+          KC_SPC, MO(_FN),
+    KC_NO, KC_NO,
+    KC_NO, KC_NO
 ),
     [_FN] = COLIN_RIGHT_DACTYL_LAYOUT(
-        KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, 
-        _______, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_F12, 
-        KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, _______, _______, 
-        _______, MO(_RS), _______, _______, _______, _______, 
-                  _______, _______, 
-          _______, _______, 
-    _______, _______, 
-    _______, _______  
+        KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11,
+        _______, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_F12,
+        KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, _______, KC_DEL,
+        _______, _______, _______, _______, _______, MO(_RS),
+                  _______, _______,
+          _______, _______,
+    _______, _______,
+    _______, _______
 ),
     [_RS] = COLIN_RIGHT_DACTYL_LAYOUT(
-        _______, _______, _______, _______, _______, _______, 
-        _______, _______, _______, _______, _______, _______, 
-        _______, RESET, _______, _______, _______, _______, 
-        _______, _______, _______, _______, _______, _______, 
-                  _______, _______, 
-          _______, _______, 
-    _______, _______, 
-    _______, _______  
+        _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______,
+        _______, RESET, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______,
+                  _______, _______,
+          _______, _______,
+    _______, _______,
+    _______, _______
     ),
 };
 
